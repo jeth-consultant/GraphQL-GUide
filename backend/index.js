e967fed1127b79a671a8529eea22f8f2e107afc1
jeth-consultant <jethrosumbeiywet@gmail.com>
2026-01-06 23:16:01 +0300

    added express middleware, also i downgraded the apollo server to v44 so that express middlware can run

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,40 @@
 import { ApolloServer } from "@apollo/server";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import { expressMiddleware } from "@apollo/server/express4";
 
-import { startStandaloneServer } from "@apollo/server/standalone";
-import mergeResolver from "./resolvers/index.js";
-import mergeTypeDef from "./typeDefs/index.js";
+import express from "express";
+import http from "http";
+import cors from "cors";
 
+import mergeResolvers from "./resolvers/index.js";
+import mergeTypeDefs from "./typeDefs/index.js";
+
+/* -------------------- APP SETUP -------------------- */
+const app = express();
+const httpServer = http.createServer(app);
+
+/* -------------------- APOLLO SERVER -------------------- */
 const server = new ApolloServer({
-  typeDefs: mergeTypeDef,
-  resolvers: mergeResolver,
+  typeDefs: mergeTypeDefs,
+  resolvers: mergeResolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
 
-const { uri } = await startStandaloneServer(server);
+await server.start();
 
-console.log(`Server ready at ${uri}`);
+/* -------------------- MIDDLEWARE -------------------- */
+app.use(
+  "/",
+  cors(),
+  express.json(),
+  expressMiddleware(server, {
+    context: async ({ req }) => ({ req }),
+  })
+);
+
+/* -------------------- START SERVER -------------------- */
+await new Promise((resolve) =>
+  httpServer.listen({ port: 4000 }, resolve)
+);
+
+console.log("🚀 Server ready at http://localhost:4000/");
